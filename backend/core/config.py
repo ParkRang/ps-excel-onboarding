157c4a6e9a1e4c834426b4e69e1684fe1057145a
박진우 <jw.park@playstory.co.kr>
2026-07-08 15:45:11 +0900

    refactor : config.py env 제거

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,23 +10,23 @@ class Settings:
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
     
-    GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-    GCP_LOCATION = os.getenv(
-        "GCP_LOCATION"
-    )
-    GCP_TASKS_QUEUE_NAME = os.getenv(
-        "GCP_TASKS_QUEUE_NAME"
-    )
+    # GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+    # GCP_LOCATION = os.getenv(
+    #     "GCP_LOCATION"
+    # )
+    # GCP_TASKS_QUEUE_NAME = os.getenv(
+    #     "GCP_TASKS_QUEUE_NAME"
+    # )
 
-    BACKEND_URL = os.getenv("BACKEND_URL")
+    # BACKEND_URL = os.getenv("BACKEND_URL")
 
-    TASKS_SERVICE_ACCOUNT_EMAIL = os.getenv(
-        "TASKS_SERVICE_ACCOUNT_EMAIL"
-    )
+    # TASKS_SERVICE_ACCOUNT_EMAIL = os.getenv(
+    #     "TASKS_SERVICE_ACCOUNT_EMAIL"
+    # )
 
-    GCP_STORAGE_BUCKET_NAME = os.getenv(
-        "GCP_STORAGE_BUCKET_NAME"
-    )
+    # GCP_STORAGE_BUCKET_NAME = os.getenv(
+    #     "GCP_STORAGE_BUCKET_NAME"
+    # )
 
     DISCORD_WEBHOOK_URL = os.getenv(
         "DISCORD_WEBHOOK_URL"
