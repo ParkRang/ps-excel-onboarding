ab5b0b514c7c2a40fc9c3ea5f9a54393ced4dc8d
박진우 <jw.park@playstory.co.kr>
2026-07-09 11:29:41 +0900

    feat : config.py 변경

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,30 +4,23 @@ from dotenv import load_dotenv
 load_dotenv() # .env 파일 로드
 
 class Settings:
+    INFRA_MODE = os.getenv("INFRA_MODE", "local")
+
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
     
-    # GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-    # GCP_LOCATION = os.getenv(
-    #     "GCP_LOCATION"
-    # )
-    # GCP_TASKS_QUEUE_NAME = os.getenv(
-    #     "GCP_TASKS_QUEUE_NAME"
-    # )
-
-    # BACKEND_URL = os.getenv("BACKEND_URL")
-
-    # TASKS_SERVICE_ACCOUNT_EMAIL = os.getenv(
-    #     "TASKS_SERVICE_ACCOUNT_EMAIL"
-    # )
-
-    # GCP_STORAGE_BUCKET_NAME = os.getenv(
-    #     "GCP_STORAGE_BUCKET_NAME"
-    # )
+    GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+    GCP_LOCATION = os.getenv("GCP_LOCATION")
+    GCP_TASKS_QUEUE_NAME = os.getenv("GCP_TASKS_QUEUE_NAME")
+    BACKEND_URL = os.getenv("BACKEND_URL")
+    TASKS_SERVICE_ACCOUNT_EMAIL = os.getenv("TASKS_SERVICE_ACCOUNT_EMAIL")
+    GCP_STORAGE_BUCKET_NAME = os.getenv("GCP_STORAGE_BUCKET_NAME")
 
     DISCORD_WEBHOOK_URL = os.getenv(
         "DISCORD_WEBHOOK_URL"
     )
+    EXCEL_STORAGE_DIR = os.getenv("EXCEL_STORAGE_DIR", "files")
+    EXCEL_DOWNLOAD_PREFIX = os.getenv("EXCEL_DOWNLOAD_PREFIX", "/files")
